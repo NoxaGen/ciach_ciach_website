4debe16d80edfbf82620cb535e66a351fbe8b4b1
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2023-02-11 14:49:39 +0100

    created separate javascript file for subpages

---
+++ uslugi/uslugi.js
@@ -0,0 +1,11 @@
+//include main.js
+
+const aboutUsSubMobile = document.querySelector('.about-us-subpage-mobile');
+const servicesSubMobile = document.querySelector('.services-subpage-mobile');
+
+aboutUsSubMobile.addEventListener('click', function () {
+    console.log('working');
+    mobileMenuBehaviour();
+    // window.location.href = "../index.html";
+    // scrollAutoMobileServices();
+});
