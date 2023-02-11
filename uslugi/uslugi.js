19de7ad6ec401ee7a3c93338ab5041ca0958bafc
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2023-02-11 23:28:49 +0100

    write script for desktops

--- uslugi/uslugi.js
+++ uslugi/uslugi.js
@@ -1,4 +1,5 @@
 //include main.js
+//mobile
 
 const aboutUsSubMobile = document.querySelector('.about-us-subpage-mobile');
 const servicesSubMobile = document.querySelector('.services-subpage-mobile');
@@ -9,6 +10,21 @@ aboutUsSubMobile.addEventListener('click', (e) => {
 });
 
 servicesSubMobile.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../index.html#services-id';
+});
+
+//desktop
+
+const aboutUsSubDesktop = document.querySelector('.about-us-subpage-desktop');
+const servicesSubDesktop = document.querySelector('.services-subpage-desktop');
+
+aboutUsSubDesktop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../index.html#opinions-id';
+});
+
+servicesSubDesktop.addEventListener('click', (e) => {
     e.preventDefault();
     window.location.href = '../index.html#services-id';
 });
