b173fcd4d7bc14e39a41712c9317b7ad8b4dda68
Wiktor Augustyniak <wiktor.augustyniak@gmail.com>
2023-02-11 17:25:25 +0100

    find the solution for channel on subpage with proper scroll

--- uslugi/uslugi.js
+++ uslugi/uslugi.js
@@ -3,9 +3,12 @@
 const aboutUsSubMobile = document.querySelector('.about-us-subpage-mobile');
 const servicesSubMobile = document.querySelector('.services-subpage-mobile');
 
-aboutUsSubMobile.addEventListener('click', function () {
-    console.log('working');
-    mobileMenuBehaviour();
-    // window.location.href = "../index.html";
-    // scrollAutoMobileServices();
+aboutUsSubMobile.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../index.html#opinions-id';
+});
+
+servicesSubMobile.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = '../index.html#services-id';
 });
